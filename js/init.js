8dfffef0598d8157c49e1e23284e58b871ad3cad
picnii <sompop@picnii.me>
2013-11-26 13:59:41 +0700

    $rootScoep Menu

--- js/init.js
+++ js/init.js
@@ -1,6 +1,23 @@
 //uncomment to do initial and set $rootScope
-/*app.run(function($rootScope) {
-	
+app.run(function($rootScope) {
+	$rootScope.menus = [];
+	$rootScope.menus['home'] = {name:"Home", url:"#/", class:"active"};
+	$rootScope.menus['foods'] = {name:"Foods", url:"#/foods", class:""};
+	$rootScope.menus['drinks'] = {name:"Drinks", url:"#/drinks", class:""};
+	$rootScope.menus['order'] = {name:"Order", url:"#/order", class:""};
+
+	$rootScope.changeMenu = function(menu)
+	{
+		$rootScope._unActiveMenu();
+		$rootScope.menus[menu].class="active"
+	}
+
+	$rootScope._unActiveMenu = function()
+	{
+		for(var i in $rootScope.menus)
+		{
+			$rootScope.menus[i].class = "";
+		}
+	}
 
 });
-*/
